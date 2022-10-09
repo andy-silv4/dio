71b95177c309af98e395f986d88092d0e35f3381
andy-silv4 <andy.silv4@yahoo.com>
2022-10-09 15:06:42 -0300

    adicionado alteracoes para solucao do desafio

--- trilha-net-api-desafio/Program.cs
+++ trilha-net-api-desafio/Program.cs
@@ -5,8 +5,13 @@ using TrilhaApiDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<OrganizadorContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+else
+    builder.Services.AddDbContext<OrganizadorContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
